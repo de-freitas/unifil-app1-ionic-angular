eb9afc16661fae185ea477d579097eef1859860c
de-freitas <claudio.g.freitas@outlook.com>
2022-04-30 20:36:32 -0300

    personalização da página 'sou-aluno'.

--- src/app/sou-aluno/sou-aluno.page.ts
+++ src/app/sou-aluno/sou-aluno.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-sou-aluno',
@@ -7,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SouAlunoPage implements OnInit {
 
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
   constructor() { }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll > code below
+      // if (data.length === 1000) {
+      //   event.target.disabled = true;
+      // }
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  }
 
   ngOnInit() {
   }
